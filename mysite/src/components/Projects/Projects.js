fdaf36536f4880a89946723377325fa6c89a909e
nic0mp <nico.mp215@gmail.com>
2022-01-08 23:31:12 -0600

    Projects section

--- mysite/src/components/Projects/Projects.js
+++ mysite/src/components/Projects/Projects.js
@@ -4,10 +4,32 @@ import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag,
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 
+const projectsDemo = [{
+  title: 'Project 1',
+  description: 'This is a really really long project description'
+},{
+  title: 'Project 2',
+  description: 'This is a really really long project description'
+},{
+  title: 'Project 3',
+  description: 'This is a really really long project description'
+},{
+  title: 'Project 4',
+  description: 'This is a really really long project description'
+}];
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section>
+    <SectionDivider/>
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {projects.map((project) => (
+        <div>
+          {project.title}
+         
+        </div>
+      ))}
+    </GridContainer>
+  </Section>
 );
 
 export default Projects;
