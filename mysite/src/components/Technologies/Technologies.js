d68de729ae0dd99d0f5904511adcb93e7456e96f
nic0mp <nico.mp215@gmail.com>
2022-01-10 12:32:25 -0600

    Technologies section

--- mysite/src/components/Technologies/Technologies.js
+++ mysite/src/components/Technologies/Technologies.js
@@ -4,9 +4,26 @@ import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="tech">
+    <SectionDivider />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+    Duis aute irure dolor in reprehenderit in voluptate
+    velit esse cillum dolore eu fugiat nulla pariatur. 
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiFirebase size="3rem"/>
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br/>
+            React.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
